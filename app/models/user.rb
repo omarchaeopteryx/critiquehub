f62720a744f19470fa417732d0bfb97e322065b8
Benjamin Kwok <email": null>
2016-12-07 15:22:00 -0800

    added create user functionality with basic styling

---
+++ app/models/user.rb
@@ -0,0 +1,10 @@
+class User < ActiveRecord::Base
+  has_many :articles
+  has_secure_password
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :username, :email, presence: true
+  validates :username, length: { minimum: 3, maximum: 25}, uniqueness: { case_sensitive: false }
+  validates :email, length: { maximum: 250 }, uniqueness: { case_sensitive: false }, format: { with: VALID_EMAIL_REGEX }
+
+end
