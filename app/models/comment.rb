9a413e02796d597e4f26a92716f0b4036fe68c0f
Benjamin Kwok <email": null>
2016-12-07 18:29:38 -0800

    add test for comment model

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,6 +2,7 @@ class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :review
 
-  validates :review_id, :user_id, :description, presnece: true
+  validates :review_id, :user_id, :description, presence: true
   validates :description, length: { minimum: 5, maximum: 100}
+  validates :user, presence: true
 end
