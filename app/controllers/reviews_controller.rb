4ff83e6479f1d5cac1430949a73c810731e61e5f
Chinh Nguyen & Omar <pair+chqnguyen.omarchaeopteryx@devbootcamp.com>
2016-12-08 18:24:13 -0800

    Add routing and form for reviews

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -7,5 +7,18 @@ class ReviewsController < ApplicationController
     render :index
   end
 
+  def new
+    @movie = Tmdb::Movie.detail(params[:id])
+    @review = Review.new
+    render :new
+  end
 
+  def create
+    @review = Review.new(review_params)
+    if @review.save
+      redirect_to review_path
+    else
+      render :new
+    end
+  end
 end
