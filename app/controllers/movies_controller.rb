5175543fd79d53cce4de97bee3a9828d70fd7f8b
Chinh Nguyen & Omar <pair+chqnguyen.omarchaeopteryx@devbootcamp.com>
2016-12-08 19:31:23 -0800

    Add reviews on movie show page

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -3,7 +3,7 @@ require 'themoviedb'
 class MoviesController < ApplicationController
 
   def index
-    
+
     @title = params[:genre]
     @genre = Tmdb::Genre.find(params[:genre])
     render :index
@@ -11,43 +11,8 @@ class MoviesController < ApplicationController
 
   def show
     @movie = Tmdb::Movie.detail(params[:id])
+    @reviews = Review.where(movie_id: params[:id])
     render :show
   end
 
-
 end
-
-
-
-
-# require 'themoviedb'
-# require 'json'
-
-
-# latest = Tmdb::Movie.now_playing
-# # x = Tmdb::Movie.find("batman").each {|object| p object.id}
-# # x.each { |movie| puts movie.title }
-# # result = x.first
-
-# @movie = Tmdb::Movie.images(414)
-
-# @movie.each {|key, value| p key}
-
-# def find_image_url(movie)
-# "https://image.tmdb.org/t/p/w500" +
-# movie["posters"].first["file_path"]
-# end
-
-# p find_image_url(@movie)
-
-# # p @movie["posters"].first["file_path"]
-# # p result.id
-
-# # batman = Tmdb::Movie.detail(186579)
-# # batmanparse = batman.to_json
-# # p batmanparse2 = JSON.parse(batmanparse)
-# # p drama = Tmdb::Genre.find("Action")
-
-
-# # latest.class
-# # p latest
